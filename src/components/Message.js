4eab293886630f2bbd3fc6e1b24aa4250e8ba2fa
Ludwig Bäcklund <ludli839@student.liu.se>
2018-08-12 00:11:57 +0200

    Update styling of messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { MessageShape } from '../features/chat/reducers';
 
+const Wrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	margin: 10px 10px 0px 10px;
+	font-family: 'Open Sans', Verdana, Helvetica, sans-serif;
+`;
+
+const Name = styled.span`
+	margin-right: 10px;
+	color: rgb(237, 66, 83);
+`;
+
 const Message = props => {
 	return (
-		<p>
-			{props.user}: {props.content}
-		</p>
+		<Wrapper>
+			<Name>{props.user}:</Name>
+			<p>{props.content}</p>
+		</Wrapper>
 	);
 };
 
