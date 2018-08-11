9c1ecbd661b6eb3473c757a0acb37c878c5d4e8f
Ludwig Bäcklund <ludli839@student.liu.se>
2018-08-12 00:12:34 +0200

    Update styling of users to look like badges

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	display: flex;
+	margin-top: 10px;
+	background-color: #f9f7f6;
+	box-shadow: rgba(0, 0, 0, 0.14) -1px 1px 2px 1px;
+	border-radius: 5px;
+`;
+
+const Name = styled.div`
+	font-family: 'Open Sans', Verdana, Helvetica, sans-serif;
+	margin: 10px;
+	width: 100%;
+	color: rgb(237, 66, 83);
+
+	@media (max-width: 900px) {
+		font-size: 0.8em;
+	}
+`;
 
 const User = ({ name }) => {
-	return <p>{name}</p>;
+	return (
+		<Wrapper>
+			<Name>{name}</Name>
+		</Wrapper>
+	);
 };
 
 User.propTypes = {
