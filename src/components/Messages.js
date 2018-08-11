af2ee641fc779809fb410fd2adb0d99fc01ed54e
Ludwig Bäcklund <ludli839@student.liu.se>
2018-08-12 00:11:30 +0200

    Add styling to message container, implement chat autoscroll

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import Message from './Message';
 
-const Messages = ({ children }) => {
-	return <div>{children}</div>;
-};
+const Wrapper = styled.div`
+	flex: 10;
+	background-color: #f9f7f6;
+	box-shadow: rgba(0, 0, 0, 0.14) -1px 1px 2px 1px;
+	border-radius: 5px;
+	overflow-y: scroll;
+`;
+
+class Messages extends Component {
+	constructor(props) {
+		super(props);
+		this.listRef = React.createRef();
+	}
+
+	getSnapshotBeforeUpdate(prevProps) {
+		if (prevProps.children.length < this.props.children.length) {
+			const list = this.listRef.current;
+			return list.scrollHeight - list.scrollTop;
+		}
+		return null;
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		if (snapshot !== null) {
+			const list = this.listRef.current;
+			list.scrollTop = list.scrollHeight - snapshot;
+		}
+	}
+
+	render() {
+		const { children } = this.props;
+		return <Wrapper innerRef={this.listRef}>{children}</Wrapper>;
+	}
+}
 
 Messages.propTypes = {
 	children: PropTypes.oneOfType([
