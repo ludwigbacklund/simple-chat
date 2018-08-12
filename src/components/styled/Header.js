ca00fcb451e6adce9ef0d8e4ca39990a9a798e6e
Ludwig Bäcklund <ludli839@student.liu.se>
2018-08-12 18:05:35 +0200

    Make Header customizable with bold and small properties

--- src/components/styled/Header.js
+++ src/components/styled/Header.js
@@ -2,14 +2,16 @@ import styled from 'styled-components';
 
 const Header = styled.span`
 	text-align: center;
-	font-family: 'Open Sans', Verdana, Helvetica, sans-serif;
+	color: white;
+	text-shadow: -1px 1px 0px rgba(0, 0, 0, 0.2);
+	font-weight: ${props => (props.bold ? '700' : '400')};
 
 	@media (max-width: 900px) {
-		font-size: 1em;
+		font-size: ${props => (props.small ? '1em' : '1.5em')};
 	}
 
 	@media (min-width: 900px) {
-		font-size: 2em;
+		font-size: ${props => (props.small ? '1.5em' : '2em')};
 	}
 `;
 
